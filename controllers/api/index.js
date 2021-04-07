997a9878bc5c5346438b8edfbe95d4a055177184
Michael Jones <mtjones1979@yahoo.com>
2021-04-07 13:16:35 -0500

    set up utils using activity 20 as template

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 // seting up routes knowing I need on for user, post, and comments
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
-const commentRoutes = require('./commentRoutes');
+const userRoutes = require('./userRoutes.js');
+// const postRoutes = require('./postRoutes.js');
+// const commentRoutes = require('./commentRoutes.js');
 
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+// router.use('/posts', postRoutes);
+// router.use('/comments', commentRoutes);
 
 
 module.exports = router;
