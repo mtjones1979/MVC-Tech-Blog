baebbf950242a18a5b816b525cd183165f2d7837
Michael Jones <mtjones1979@yahoo.com>
2021-04-09 12:55:58 -0500

    updated homeRoute.js

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -2,7 +2,7 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    const needed_funding = document.querySelector('input[name="post-content"]').value.trim();
+    const content = document.querySelector('input[name="post-content"]').value.trim();
   
     const response = await fetch(`/api/posts`, {
         method: 'POST',
