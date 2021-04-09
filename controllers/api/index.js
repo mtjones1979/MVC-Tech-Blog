b5e1fe856eb9d42f5ed11e149dd2736d7387265e
Michael Jones <mtjones1979@yahoo.com>
2021-04-09 14:26:37 -0500

    comment path works for seeded info in isomnia, now i have to build out homepage, dashboard, etc to render info

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes.js');
 const postRoutes = require('./postRoutes.js');
-// const commentRoutes = require('./commentRoutes.js');
+const commentRoutes = require('./commentRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-// router.use('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 
 
 module.exports = router;
